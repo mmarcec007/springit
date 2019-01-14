4eb7fa0f22115441ab5e7dfc262239f2e6dfa26c
mmarcec007 <mark.marcec@gmail.com>
2019-01-14 20:31:29 +0100

    [spring-security] security on the view layer

--- src/main/java/com/mmarcec/springit/security/SecurityConfiguration.java
+++ src/main/java/com/mmarcec/springit/security/SecurityConfiguration.java
@@ -29,9 +29,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/").permitAll()
             .and()
             .formLogin()
-            .and()
+                .loginPage("/login").permitAll()
+                .usernameParameter("email")
+                .and()
+            .logout()
+                .and()
+            .rememberMe()
+            /*.and()
                 .csrf().disable()
-                .headers().frameOptions().disable();
+                .headers().frameOptions().disable()*/;
     }
 
     @Override
